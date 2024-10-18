3ad3ec3fd576d12ebc42762c95b641beb017a2be
Andreas054 <84636763+Andreas054@users.noreply.github.com>
2024-10-18 11:10:20 +0300

    NPC patrol + navmesh
    Enemy prefab (Enemy + State Machine scripts)
    
    Installed package
    AI Navigation
    
    Window > AI > Navigation (obsolete)

---
+++ Game/Assets/Scripts/Enemy/StateMachine.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public BaseState activeState;
+    public PatrolState patrolState;
+
+    public void Initialize()
+    {
+        patrolState = new PatrolState();
+        ChangeState(patrolState);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeState != null)
+        {
+            activeState.Perform();
+        }
+    }
+
+    public void ChangeState(BaseState newState)
+    {
+        if (activeState != null)
+        {
+            activeState.Exit();
+        }
+        // change to new state
+        activeState = newState;
+
+        if (activeState != null)
+        {
+            activeState.stateMachine = this;
+            activeState.enemy = GetComponent<Enemy>();
+            activeState.Enter();
+        }
+    }
+}
