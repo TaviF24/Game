f2344a8883b497be07337287a89ef14eb81bac0f
TaviF24 <118764142+TaviF24@users.noreply.github.com>
2024-12-28 15:37:48 +0200

    done npc spawner

--- Game/Assets/Scripts/Player/PlayerHealth.cs
+++ Game/Assets/Scripts/Player/PlayerHealth.cs
@@ -106,14 +106,14 @@ public class PlayerHealth : MonoBehaviour, IDamageable, IDataPersistence
 
 	public void TakeDamage(float damage)
 	{
-		//health -= damage;
+		health -= damage;
 		lerpTimer = 0;
 		UpdateHealthUI();
 		durationTimer = 0;
 		fadeSpeed = 1.5f;
 		overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 1);
 
-        if (health <= 0)
+        if (health <= 0 && !deathScreen.activeInHierarchy)
         {
             StartCoroutine(HandleDeath());
         }
@@ -125,20 +125,19 @@ public class PlayerHealth : MonoBehaviour, IDamageable, IDataPersistence
 
 		playerShoot.BlockShooting(true);
         deathScreen.SetActive(true); // show death screen
-
         yield return new WaitForSeconds(deathScreenDuration);
 
         SceneManager.instance.targetPosition = targetPosition;
         SceneManager.instance.NextScene(nextScene);
-        RestoreHealth(maxHealth);
+        RestoreHealth();
 
         playerShoot.BlockShooting(false);
         deathScreen.SetActive(false); // hide death screen
     }
 
-    public void RestoreHealth(float healAmount)
+    public void RestoreHealth()
 	{
-		health += healAmount;
+		health = maxHealth;
 		lerpTimer = 0;
 		UpdateHealthUI();
 	}
